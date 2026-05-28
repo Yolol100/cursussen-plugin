8f8e4af8953a33d2f7d0c40f434e8e5b74e7db63
Yolol100 <Andrew_Baeten@hotmail.com>
2026-05-29 05:08:04 +0700

    Update tested up to WordPress 7.0

--- cursussen-plugin.php
+++ cursussen-plugin.php
@@ -7,7 +7,7 @@
  * Text Domain: cursussen-plugin
  * Domain Path: /languages
  * Requires at least: 6.3
- * Tested up to: 6.7
+ * Tested up to: 7.0
  * Requires PHP: 7.4
  * License: GPL-2.0-or-later
  * License URI: https://www.gnu.org/licenses/gpl-2.0.html
